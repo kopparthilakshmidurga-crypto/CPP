b975343716ab5d29e545e7db914ae29aa5dced7c
kopparthilakshmidurga-crypto <kopparthilakshmidurga@gmail.com>
2026-07-20 11:41:43 +0530

    Add files via upload

---
+++ namespacealias.cpp(13).cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+namespace VeryLongNamespaceName
+{
+    void display()
+    {
+        cout << "Namespace alias example." << endl;
+    }
+}
+namespace VLN = VeryLongNamespaceName;
+int main()
+{
+    VLN::display();
+
+    return 0;
+}
