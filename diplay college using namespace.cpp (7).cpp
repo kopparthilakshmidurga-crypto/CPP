fc3643475b4d0b52b5e7ff6b1238e7003acac2cd
kopparthilakshmidurga-crypto <kopparthilakshmidurga@gmail.com>
2026-07-20 11:39:50 +0530

    Add files via upload

---
+++ diplay college using namespace.cpp (7).cpp
@@ -0,0 +1,15 @@
+#include<iostream>
+using namespace std;
+namespace College
+{
+	void displayCollege()
+	{
+		cout<<"Welcome to Aditya University"<<endl;
+	}
+}
+int main()
+{
+	
+	College::displayCollege();
+	return 0;
+}
