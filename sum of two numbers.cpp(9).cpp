b4028ace63e62608ac3dab540bbc5d03d08de635
kopparthilakshmidurga-crypto <kopparthilakshmidurga@gmail.com>
2026-07-20 11:43:17 +0530

    Add files via upload

---
+++ sum of two numbers.cpp(9).cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int a, b, sum;
+	cout <<"Enter two integers: ";
+    cin >>a>>b;
+    sum = a + b;
+    cout << "Sum = " << sum << endl;
+
+    return 0;
+}
