2632cb423b1a674b042f4a55028f54029c4af058
kopparthilakshmidurga-crypto <kopparthilakshmidurga@gmail.com>
2026-07-28 17:19:42 +0530

    Add files via upload

---
+++ swapping of two numbers.cpp(21).cpp
@@ -0,0 +1,39 @@
+#include<iostream>
+#include<string>
+using namespace std;
+class B;
+class A
+{
+	int a;
+	public:
+	A() 
+	{
+	a=10;
+	}
+	friend void swapping(A &,B &);
+};
+class B
+{
+	int b;
+	public:
+	B()
+	{
+	b=20;
+	}
+	friend void swapping(A &,B &);
+};
+void swappig(A &x,B &y)
+{
+	int temp;
+	temp=x.a;
+	x.a=y.b;
+	y.b=temp;
+	cout<<"After swapping: "<<"a: "<<x.a<<"b: "<<y.b<<endl;
+}
+int main()
+{
+	A a1;
+	B b1;
+	swapping(a1,b1);
+	return 0;
+}
